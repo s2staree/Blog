6b30d99422c7875e9eb651e8f7d6079f297f74c9
s2staree <s2staree@gmail.com>
2022-09-08 17:34:43 +0900

    17:00 수업 저장 - 코드 수정 리팩토링(글 수정 완료, xss 실패)

--- src/main/java/site/metacoding/red/web/dto/request/users/JoinDto.java
+++ src/main/java/site/metacoding/red/web/dto/request/users/JoinDto.java
@@ -1,8 +1,12 @@
 package site.metacoding.red.web.dto.request.users;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@NoArgsConstructor
+@AllArgsConstructor
 @Setter
 @Getter
 public class JoinDto {
